ac9233504f68ec7bc96498f7f28364b6618e6487
rei <idareiya123@gmail.com>
2025-10-05 10:11:53 +0900

    add: パスワード保存・出力処理

--- password_manager.sh
+++ password_manager.sh
@@ -1,7 +1,43 @@
 #!/bin/bash
 echo "パスワードマネージャーへようこそ！"
-read -p "サービス名を入力してください： " servicename
-read -p "ユーザー名を入力してください： " username
-read -p "パスワードを入力してください： " password
-echo "$servicename:$username:$password" >> information.txt
-echo "Thank you!"
+
+i=0
+while [ "$i" = 0 ]; do
+    read -p "次の選択肢から入力してください(Add Password/Get Password/Exit)：" Action Password
+    choice="${Action}${Password}"
+
+    # ファイルへ保存
+    if [ "$choice" = "AddPassword" ]; then
+        read -p "サービス名を入力してください： " servicename
+        read -p "ユーザー名を入力してください： " username
+        read -p "パスワードを入力してください： " password
+        echo "$servicename:$username:$password" >> information.txt
+        echo "パスワードの追加は成功しました。"
+
+    # パスワードの取得と表示
+    elif [ "$choice" = "GetPassword" ]; then
+        read -p "サービス名を入力してください： " servicename
+        while read line
+        do
+            service=$(echo "$line" | cut -d':' -f1)
+            user=$(echo "$line" | cut -d':' -f2)
+            pass=$(echo "$line" | cut -d':' -f3)
+
+            if [ "$servicename" = "$service" ]; then
+                echo "サービス名: ${service}"
+                echo "ユーザー名: ${user}"
+                echo "パスワード: ${pass}"
+                break
+            fi
+        done < "information.txt"
+
+    # 終了
+    elif [ "$choice" = "Exit" ]; then
+        i=1
+        echo "Thank you!"
+
+    # 入力が間違えている場合
+    else
+        echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
+    fi
+done
